c6b8f69adbc44ed79f8c0536e9b3bd478701b4f5
mxriaaw <naryanesantos1@gmail.com>
2025-11-11 16:46:59 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import Styles from './App.module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+ const [n1, setN1] = useState()
+ const [n2, setN2] = useState()
+ const [respSoma, setRespSoma] = useState()
+ const [respSub, setRespsub] = useState()
+ const [respMult, setRespMult] = useState()
+ const [respDiv, setRespDiv] = useState()
+
+ const[error, setError] = useState()
+
+
+ useEffect(() =>{
+  if(n1 !== undefined && n2 !== undefined && !isNaN(n1)){
+ setRespSoma(parseFloat(n1) + parseFloat(n2))
+
+ setError('')
+  }else{
+    setError('digite valores numericos para calcular')
+  }
+ }, [n1, n2])
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+   <h1 className={Styles.title}>qualquer</h1>
+   <p>{error}</p>
+    <div className={Styles.wrap}>
+      <input type="number" onChange={(e) => setN1(e.target.value)} value={''} placeholder=''/>
+      <input type="number" onChange={(e) => setN2 (e.target.value)} value={''} placeholder=''/>
+    </div>
+
+    <div className={Styles.wrapResps}>
+    <h2>resultado da soma</h2>
+    <h3>{respSoma}</h3>
+    </div>
+
+    <div className={Styles.wrapResps}>
+    <h2>resultado da subtracao</h2>
+    <h3>{respSub}</h3>
+    </div>
+
+    <div className={Styles.wrapResps}>
+      <h2>resultado da multiplicacao</h2>
+      <h3>{respMult}</h3>
+    </div>
+
+    <div className={Styles.wrapResps}>
+      <h2>resultado da divisao</h2>
+      <h3>{respDiv}</h3>
+
+    </div>
     </>
   )
 }
